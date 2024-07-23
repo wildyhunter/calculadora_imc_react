e8117df15d4c4f40c9f523b81ba57e2c1bb4b95a
VAGNER OLIVEIRA <116770973+wildyhunter@users.noreply.github.com>
2024-07-23 16:09:10 +0100

    Inicio do projeto

---
+++ imc_calc/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ImcCal from './components/ImcCal'
+
+
+function App() {
+
+  return (
+    <div className='Container'>
+      <ImcCal/>
+    </div>
+  )
+}
+
+export default App
